afcc126a8ba8f571cdd9beb8efd67ac2f9772b9b
alienemperor <joshua.mckinnon.10@gmail.com>
2018-02-27 16:00:55 -0700

    emerging tech, finished beginner tutorial

--- testSite/polls/models.py
+++ testSite/polls/models.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+import datetime
 # Create your models here.
 from django.db import models
 from django.utils import timezone
@@ -15,7 +15,11 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
